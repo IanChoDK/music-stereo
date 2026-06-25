104db67fb8f0d8ea889efc159be00aece040c806
mchena <m.chena@itecriocuarto.org.ar>
2026-06-24 22:09:06 -0300

    Ruta corregida

--- src/app/albums/[id]/page.jsx
+++ src/app/albums/[id]/page.jsx
@@ -1,40 +1,15 @@
 "use client";
-//import { useAlbums } from "@/context/AlbumsContext";
+
+import { useAlbums } from "@/context/AlbumsContext";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import AlbumDetail from "@/app/components/AlbumDetail";
-import axios from "axios";
 
 export default function AlbumPage() {
-  // Obtener id
   const { id } = useParams();
-  // const { getAlbumById, deleteAlbum } = useAlbums();
+  const { getAlbumById, deleteAlbum } = useAlbums();
   const router = useRouter();
-  // Estados para controlar carga y album
-  const [Loading, setLoading] = useState(true);
-  const [album, setAlbum] = useState(null);
 
-  // Funcion para obtener datos del album
-  const fetchAlbum = async () => {
-    setLoading(true);
-
-    try {
-      // Llamada a la API
-      const response = await axios.get(`/api/album/${id}`);
-      setAlbum(response.data);
-    } catch (error) {
-      console.error("Error al obtener informacion sobre el album", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Cargar informacion del album cada vez que cambia el ID
-  useEffect(() => {
-    if (id) {
-      fetchAlbum();
-    }
-  }, [id]);
+  const album = getAlbumById(id);
 
   const handleDelete = () => {
     if (confirm("¿Estás seguro de que quieres eliminar este álbum?")) {
@@ -43,17 +18,7 @@ export default function AlbumPage() {
     }
   };
 
-  // Mostrar mensaje de carga
-  if (Loading) {
-    return (
-      <div className="flex flex-1 justify-center items-center text-white min-h-screen">
-        <h1 className="text-3xl font-bold animate-pulse">Cargando álbum...</h1>
-      </div>
-    );
-  }
-
-  // Mostrar mensaje de error
-  if (!album || album.error) {
+  if (!album) {
     return (
       <div className="flex flex-1 justify-center items-center text-white">
         <h1 className="text-3xl font-bold">Álbum no encontrado</h1>
